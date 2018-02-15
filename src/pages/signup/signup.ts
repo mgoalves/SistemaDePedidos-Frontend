9f54ac2b4dc4e6828c8142012497a6d2b57f4250
Marcello Alves <marcello_doalves@hotmail.com>
2018-02-15 09:41:11 -0200

    Criando formGroup para validações do formulário.

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @IonicPage()
 @Component({
@@ -8,9 +9,38 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class SignupPage {
 
+  //Atributos ----------------
+  formGroup: FormGroup;
+
 
   //Construtor ----------------
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    public formBuilder: FormBuilder) {
+
+
+      this.formGroup = this.formBuilder.group({
+
+        nome: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(40)]],
+        email: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(40), Validators.email]],
+        cpfOuCnpj: ['', [Validators.required, Validators.minLength(9), Validators.maxLength(14)]],
+        senha: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(14)]],
+
+        tipo: ['', [Validators.required]],
+
+        logradouro: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(40)]],
+        numero: [null, [Validators.minLength(1), Validators.maxLength(5)]],
+        complemento: ['', [Validators.maxLength(60)]],
+        bairro: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(40)]],
+        cep: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(8)]],
+        estadoId : [null, [Validators.required]],
+        cidadeId : [null, [Validators.required]],
+        
+        telefone1: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(14)]],
+        telefone2: [null, [Validators.minLength(8), Validators.maxLength(14)]]
+      });
+
   }
 
 
